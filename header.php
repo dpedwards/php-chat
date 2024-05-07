3c004242c8b26ca18ebb7e801a55564a148a1b16
Davain Edwards <core8@gmx.net>
2024-05-07 13:44:22 +0200

    :up: Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Simple MySQL + PHP Realtime Chat App | Davain Pablo Edwards</title>
-  <link rel="stylesheet" href="style.css">
+  <title>MySQL + PHP Realtime Chat</title>
+  <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
 </head>
